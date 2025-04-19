2b030e0e975914f73ec351edeb1b4bd2df021966
Nino <tekabu0622@gmail.com>
2025-04-19 18:35:12 +0800

    add test login

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 
+use App\Models\User;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -30,5 +32,13 @@ class LoginController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/');
+    }
+    public function test()
+    {
+        $user = User::find(1);
+
+        Auth::login($user);
+
+        return redirect(route('dashboard'));
     }
 }
